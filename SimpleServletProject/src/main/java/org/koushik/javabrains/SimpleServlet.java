db999a58f63adc04e41618e2485455482c80e0e0
sourceTree <sourcetree@mail.com>
2024-01-12 07:19:53 -0700

    08 - Request, Session and Context Part 2

--- SimpleServletProject/src/main/java/org/koushik/javabrains/SimpleServlet.java
+++ SimpleServletProject/src/main/java/org/koushik/javabrains/SimpleServlet.java
@@ -1,11 +1,11 @@
 package org.koushik.javabrains;
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -18,10 +18,13 @@ public class SimpleServlet extends HttpServlet {
 		PrintWriter writer = response.getWriter();
 		String uName = request.getParameter("userName");
 		HttpSession session = request.getSession();
+		ServletContext context = request.getServletContext();
 		if(uName != "" && uName != null) {
 			session.setAttribute("savedUserName", uName);
+			context.setAttribute("savedUserName", uName);
 		}		
 		writer.println("Request parameter has username as " + uName + "<br>");
-		writer.println("Session parameter has username as " + (String)session.getAttribute("savedUserName"));
+		writer.println("Session parameter has username as " + (String)session.getAttribute("savedUserName") + "<br>");
+		writer.println("Context parameter has username as " + (String)context.getAttribute("savedUserName"));
 	}
 }
