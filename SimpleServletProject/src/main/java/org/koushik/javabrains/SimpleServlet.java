a3013debf07f7457490057971286a9275a094c23
sourceTree <sourcetree@mail.com>
2024-01-11 21:37:19 -0700

    02 - First Servlet Part 2

---
+++ SimpleServletProject/src/main/java/org/koushik/javabrains/SimpleServlet.java
@@ -0,0 +1,19 @@
+package org.koushik.javabrains;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+//@WebServlet(description = "A simple servlet", urlPatterns = {"/SimpleServletPath"})
+public class SimpleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Hello from GET method");
+		PrintWriter writer = response.getWriter();
+		writer.println("<h3>Hello in html</h3>");
+	}
+}
