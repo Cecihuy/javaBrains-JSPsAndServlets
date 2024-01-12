7a330788809c5110e6788869e2abe0b493c984bf
sourceTree <sourcetree@mail.com>
2024-01-12 01:53:02 -0700

    05 - The POST Method and Passing Parameters

--- SimpleServletProject/src/main/java/org/koushik/javabrains/XmlServlet.java
+++ SimpleServletProject/src/main/java/org/koushik/javabrains/XmlServlet.java
@@ -1,10 +1,21 @@
 package org.koushik.javabrains;
+import java.io.IOException;
+import java.io.PrintWriter;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class XmlServlet extends HttpServlet {
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-		System.out.println("XML servlet called!");
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.setContentType("text/html");
+		PrintWriter printWriter = response.getWriter();
+		String uName = request.getParameter("userName");
+		printWriter.println("Hello from GET method! " + uName);
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.setContentType("text/html");
+		PrintWriter printWriter = response.getWriter();
+		String uName = request.getParameter("userName");
+		printWriter.println("Hello from POST method! " + uName);
 	}
 }
