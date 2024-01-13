d312bebdc90073b22f4d147baab6da98a25fb136
sourceTree <sourcetree@mail.com>
2024-01-12 23:06:33 -0700

    16 Part 2- Writing an MVC Application

--- LoginApp/src/main/java/org/koushik/javabrains/LoginServlet.java
+++ LoginApp/src/main/java/org/koushik/javabrains/LoginServlet.java
@@ -6,6 +6,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import org.koushik.javabrains.service.LoginService;
+
 @WebServlet(urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -15,7 +17,14 @@ public class LoginServlet extends HttpServlet {
 		
 		userId = request.getParameter("userId");
 		password = request.getParameter("password");
-		
-		
+		LoginService loginService = new LoginService();
+		boolean result = loginService.authenticate(userId, password);
+		if(result) {
+			response.sendRedirect("success.jsp");
+			return;
+		} else {
+			response.sendRedirect("login.jsp");
+			return;
+		}
 	}
 }
